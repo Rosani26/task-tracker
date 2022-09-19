dbb28342d0f97d2aa45f63e7a909eab23ec5bce9
Selvaratnam Rosani, GHR-OSA-NEX-21 <Rosani.Selvaratnam@swisscom.com>
2022-09-19 09:26:53 +0200

    old folders

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import propTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title, onAdd}, showAdd) => {
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color={showAdd? 'red':'green'} text={showAdd ? 'Close': 'Add'} OnClick= {onAdd}
+      />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: propTypes.string.isRequired,
+}
+
+//Css in Js
+/* const headingStyle = {
+  color: 'red', 
+  backgroundColor: 'black'
+} */
+
+export default Header
